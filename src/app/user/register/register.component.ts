4847dd5717230e4cc8ef0dbab81cf994bb6e9e5e
Julio Salas <jscomic@gmail.com>
2022-08-01 22:53:31 -0500

    validators for password and async email placed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -11,31 +13,38 @@ import IUser from 'src/app/models/user.model';
 
 //for using forms
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   inSubmission = false;
 
-  registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl<number | null>(null, [
-      Validators.required,
-      Validators.min(18),
-      Validators.max(120),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email],
+        [this.emailTaken.validate]
       ),
-    ]),
-    confirm_password: new FormControl('', [Validators.required]),
-    phoneNumber: new FormControl('', [
-      Validators.required,
-      Validators.minLength(13),
-      Validators.maxLength(13),
-    ]),
-  });
+      age: new FormControl<number | null>(null, [
+        Validators.required,
+        Validators.min(18),
+        Validators.max(120),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+        ),
+      ]),
+      confirm_password: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(13),
+        Validators.maxLength(13),
+      ]),
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   showAlert = false;
   alertMsg = 'Please Wait! You account is being created.';
